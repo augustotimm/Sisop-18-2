baad001c5f5ae97100770571004bf7c792c4d6ba
Augusto <aluno@alunovm-sisop.(none)>
2018-09-06 15:16:49 -0300

    criada funcao de inicializacao da fila do semaforo da cpu e funcao auxiliar para imprimar o semaforo

--- cthread/src/lib.c
+++ cthread/src/lib.c
@@ -1,12 +1,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "../include/support.h"
 #include "../include/cthread.h"
 #include "../include/cdata.h"
 
 
 int ccreate (void* (*start)(void*), void *arg, int prio) {
+    TCB_t * newThread = (TCB_t *) malloc(sizeof(TCB_t));
+    newThread->tid =0; //0 tem que ser substituido por uma funçao que retorne o tid
+    newThread->state =0; //0 tem que ser substituido por uma funcao do escalonador que va verificar em qual estado deve entrar
+    newThread->prio= prio;
+    getcontext(&(newThread->context));
+
 	return -1;
 }
 
@@ -23,7 +30,9 @@ int cjoin(int tid) {
 }
 
 int csem_init(csem_t *sem, int count) {
-	return -1;
+    sem->count = count;
+    int success = CreateFila2(sem->fila);
+	return success;
 }
 
 int cwait(csem_t *sem) {
@@ -38,5 +47,4 @@ int cidentify (char *name, int size) {
 	strncpy (name, "Sergio Cechin - 2017/1 - Teste de compilacao.", size);
 	return 0;
 }
-
 
